70f0436d43a524633ff93c629088b1862c665a8c
Athfan Khaleel <athphane@users.noreply.github.com>
2019-07-25 21:07:54 +0500

    updated timers

--- timers.php
+++ timers.php
@@ -6,19 +6,39 @@ $timers = [
         'date' => "Jan 17, 2019 00:00:00",
         'up' => True,
     ],
-    "A.I Assignment Deadline" => [
+    "A.I Assignment" => [
         'id' => "aiAssignmentDeadline",
         'date' => "Aug 21, 2019 00:00:00",
         'up' => False,
     ],
-    "OOSD Assignment Deadline" => [
+    "OOSD Assignment" => [
         'id' => "oosdAssignmentDeadline",
         'date' => "Aug 20, 2019 00:00:00",
         'up' => False,
     ],
-    "CNS Chimera Project" => [
-        'id' => "cnsChimeraProject",
+    "CNS Assignment 1" => [
+        'id' => "cnsassignment1",
+        'date' => "Aug 8, 2019 00:00:00",
+        'up' => False,
+    ],
+    "POC E-Assessment 1" => [
+        'id' => "poceassessment1",
+        'date' => "Jul 31, 2019 00:00:00",
+        'up' => False,
+    ],
+    "AI Exam" => [
+        'id' => "aiexam",
+        'date' => "Aug 4, 2019 00:00:00",
+        'up' => False,
+    ],
+    "POC Exam" => [
+        'id' => "pocexam",
         'date' => "Aug 6, 2019 00:00:00",
+        'up' => False,
+    ],
+    "CNS Exam" => [
+        'id' => "cnsexam",
+        'date' => "Aug 19, 2019 00:00:00",
         'up' => False,
     ],
 ];
