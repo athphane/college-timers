fe1582867feec932e07b4ac1233c71516351cfce
MohamedAthfanKhaleel <athphane@gmail.com>
2019-12-22 23:07:46 +0500

    - TAILWIND

--- index.php
+++ index.php
@@ -1,95 +1,81 @@
-<?php 
+<?php
 include 'timers.php';
 ?>
+<!doctype html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <link rel="icon" href="favicon.png">
-
-  <title>College Timers</title>
-
-  <!-- Bootstrap core CSS -->
-  <link rel="stylesheet" href="assets/bootstrap.min.css">
-
-  <!-- Custom styles for this template -->
-  <link href="assets/cover.css" rel="stylesheet">
-  <link href="assets/index.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <title>College Timers</title>
 </head>
-
-<body class="text-center">
-
-  <div class="d-flex w-100 h-100 p-3 mx-auto flex-column">
-    <header class="masthead mb-auto">
-    </header>
-    <main role="main" class="inner cover">
-        <div class="row">
-            <?php
-                foreach($timers as $key => $data) {
-            ?>
-                    <div class="col-md">
-                        <h3><?php echo($key) ?></h3>
-                        <hr>
-                        <h3 id="<?php echo($data['id']) ?>" class="cover-heading"></h1>
-                    </div>
-            <?php
-                }
-            ?>
-      </div>
-    </main>
-
-    <footer class="mastfoot mt-auto">
-      <div class="inner">
-        <p><strong>Copyright &copy;
-            <script type="text/javascript">
-              document.write(new Date().getFullYear());
-            </script>
-            <a href="https://athfan.com"> Athfan Khaleel</a>.
-          </strong> All rights reserved.</p>
-      </div>
-    </footer>
-  </div>
-
-  <!-- Bootstrap core JavaScript
-    ================================================== -->
-  <!-- Placed at the end of the document so the pages load faster -->
-  <script src="assets/jquery-3.3.1.slim.min.js" type="-text/javascript"></script>
-
-    <?php 
-        foreach($timers as $key => $data) { 
+<body class="antialiased bg-blue-400">
+    <?php
+    foreach ($timers as $timer) {
     ?>
-        <script>
-            var <?php echo("to_".$data['id']) ?> = new Date("<?php echo($data['date']) ?>").getTime();
-            var x = setInterval(function() {
-                var now = new Date().getTime();
-                
-                // Find the difference between the current time and the toDate time..
-                <?php
-                    if($data['up']) {
-                ?>
-                    var distance = now - <?php echo("to_".$data['id']) ?>;
-                <?php
-                    } else{
-                ?>
-                    var distance = <?php echo("to_".$data['id']) ?> - now;
-                <?php
-                    }
-                ?>
+    <div class="md:flex px-6">
+        <?php
+        foreach ($timer as $key => $data) {
+            ?>
+            <div class="mx-auto rounded p-6 md:w-1/3">
+                <div class="rounded bg-white shadow-md h-48 p-6 flex flex-col justify-around uppercase">
+                    <div>
+                        <p class="text-2xl text-base font-bold text-grey-dark text-center"><?php echo($key) ?></p>
+                    </div>
+                    <div>
+                        <p id="<?php echo($data['id']) ?>" class="text-3xl text-red-500 font-bold text-center">
+                            Loading...
+                        </p>
+                    </div>
 
-                // Time calculations for days, hours, minutes and seconds
-                var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-                var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-                // Output the result in an element with it's representive id
-                document.getElementById("<?php echo($data['id']) ?>").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-            }, 1000);
-        </script>
+                </div>
+            </div>
+        <?php
+        }
+        ?>
+    </div>
     <?php
     }
     ?>
+<?php
+foreach ($timers as $timer) {
+?>
+<?php
+foreach ($timer as $key => $data) {
+    ?>
+    <script>
+        var <?php echo("to_" . $data['id']) ?> =
+        new Date("<?php echo($data['date']) ?>").getTime();
+        var x = setInterval(function () {
+            var now = new Date().getTime();
+
+            // Find the difference between the current time and the toDate time..
+            <?php
+            if($data['up']) {
+            ?>
+            var distance = now - <?php echo("to_" . $data['id']) ?>;
+            <?php
+            } else{
+            ?>
+            var distance = <?php echo("to_" . $data['id']) ?> -now;
+            <?php
+            }
+            ?>
+
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            // Output the result in an element with it's representive id
+            document.getElementById("<?php echo($data['id']) ?>").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+        }, 1000);
+    </script>
+    <?php
+}
+?>    <?php
+}
+?>
 </body>
 </html>
--- timers.php
+++ timers.php
@@ -1,29 +1,34 @@
 <?php
 
 $timers = [
-    "Days Since Villa College" => [
-        'id' => "daysSinceVilla",
-        'date' => "Jan 17, 2019 00:00:00",
-        'up' => True,
+    [
+        "Days Since Villa" => [
+            'id' => "daysSinceVilla",
+            'date' => "Jan 17, 2019 17:30:00",
+            'up' => True,
+        ],
+        "A.I Assignment" => [
+            'id' => "aiAssignmentDeadline",
+            'date' => "Aug 21, 2019 00:00:00",
+            'up' => False,
+        ],
+        "OOSD Assignment" => [
+            'id' => "oosdAssignmentDeadline",
+            'date' => "Aug 20, 2019 00:00:00",
+            'up' => False,
+        ],
     ],
-    "A.I Assignment" => [
-        'id' => "aiAssignmentDeadline",
-        'date' => "Aug 21, 2019 00:00:00",
-        'up' => False,
+    [
+        "CNS Assignment 1" => [
+            'id' => "cnsassignment1",
+            'date' => "Aug 8, 2019 00:00:00",
+            'up' => False,
+        ],
+        "CNS Exam" => [
+            'id' => "cnsexam",
+            'date' => "Aug 19, 2019 00:00:00",
+            'up' => False,
+        ],
     ],
-    "OOSD Assignment" => [
-        'id' => "oosdAssignmentDeadline",
-        'date' => "Aug 20, 2019 00:00:00",
-        'up' => False,
-    ],
-    "CNS Assignment 1" => [
-        'id' => "cnsassignment1",
-        'date' => "Aug 8, 2019 00:00:00",
-        'up' => False,
-    ],
-    "CNS Exam" => [
-        'id' => "cnsexam",
-        'date' => "Aug 19, 2019 00:00:00",
-        'up' => False,
-    ],
+
 ];
