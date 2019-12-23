d9f7f2942eea5006ff3a2048f286252ba94e5ca0
MohamedAthfanKhaleel <athphane@gmail.com>
2019-12-23 12:06:37 +0500

    - remove old timers

--- timers.php
+++ timers.php
@@ -7,28 +7,5 @@ $timers = [
             'date' => 'Jan 17, 2019 17:30:00',
             'up'   => true,
         ],
-        'A.I Assignment' => [
-            'id'   => 'aiAssignmentDeadline',
-            'date' => 'Aug 21, 2019 00:00:00',
-            'up'   => false,
-        ],
-        'OOSD Assignment' => [
-            'id'   => 'oosdAssignmentDeadline',
-            'date' => 'Aug 20, 2019 00:00:00',
-            'up'   => false,
-        ],
     ],
-    [
-        'CNS Assignment 1' => [
-            'id'   => 'cnsassignment1',
-            'date' => 'Aug 8, 2019 00:00:00',
-            'up'   => false,
-        ],
-        'CNS Exam' => [
-            'id'   => 'cnsexam',
-            'date' => 'Aug 19, 2019 00:00:00',
-            'up'   => false,
-        ],
-    ],
-
 ];
