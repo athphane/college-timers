abffb0e8561b18679c58bd4139e279f2b0b9063a
Athfan Khaleel <athphane@users.noreply.github.com>
2019-12-27 15:49:12 +0500

    Update timers.php

--- timers.php
+++ timers.php
@@ -4,7 +4,7 @@ $timers = [
     [
         'Days Since Villa' => [
             'id'   => 'daysSinceVilla',
-            'date' => 'Jan 17, 2019 17:30:00',
+            'date' => 'Jan 17, 2018 17:30:00',
             'up'   => true,
         ],
     ],
