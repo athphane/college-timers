72fbb6de04890b0f50ad66f2408e696ee7f3aaaf
Athfan Khaleel <athphane@gmail.com>
2019-12-31 10:09:42 +0000

    Apply fixes from StyleCI

--- timers.php
+++ timers.php
@@ -3,7 +3,7 @@
 $timers = [
     [
         'Days Since Villa' => [
-        'id' => 'daysSinceVilla',
+        'id'   => 'daysSinceVilla',
         'date' => 'Jan 17, 2019 17:30:00',
         'up'   => true,
         ],
