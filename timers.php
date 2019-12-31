2816997e3c33bceb3070a528226d8eebdbc274e1
Athfan Khaleel <athphane@users.noreply.github.com>
2019-12-31 15:05:36 +0500

    Update timers.php

--- timers.php
+++ timers.php
@@ -7,6 +7,11 @@ $timers = [
                 'date' => 'Jan 17, 2019 17:30:00',
                 'up'   => true,
         ],
+        'Birthday' => [
+            'id'       => 'birthday',
+                'date' => 'Apr 09, 2020 00:00:00',
+                'up'   => true,
+        ],
 
     ],
 ];
