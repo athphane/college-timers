9ba140d1d32064a2130608548126d8133a80f0d0
Athfan Khaleel <athphane@users.noreply.github.com>
2020-02-02 12:53:14 +0500

    Update timers.php

--- timers.php
+++ timers.php
@@ -8,4 +8,11 @@ $timers = [
         'up'   => true,
         ],
     ],
+	[
+        'Days Since Villaa' => [
+        'id'   => 'daysSinceVillaa',
+        'date' => 'Jan 17, 2019 17:30:00',
+        'up'   => true,
+        ],
+    ],
 ];
