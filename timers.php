2026141cf5f510cb616dcd640ec59edcb3dde15b
Athfan Khaleel <athphane@gmail.com>
2021-03-01 14:21:47 +0500

    - update times

--- timers.php
+++ timers.php
@@ -7,11 +7,5 @@ $timer_groups = [
             'date' => 'Jan 17, 2019 17:30:00',
             'up'   => true,
         ],
-
-        'Days Until Sem 3' => [
-            'id'   => 'daysUntilSemThree',
-            'date' => 'Jan 30, 2021 18:00:00',
-            'up'   => false,
-        ],
     ],
 ];
