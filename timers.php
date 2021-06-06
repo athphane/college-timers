3c76246ad710af6e4d0fa909bcb979af17c9958a
Athfan Khaleel <athphane@gmail.com>
2021-06-06 12:14:25 +0500

    - 2021 update

--- timers.php
+++ timers.php
@@ -9,21 +9,65 @@ $timer_groups = [
         ],
     ],
     [
-        'ESD Sprint 1' => [
-            'id'   => 'esdSprint1',
-            'date' => 'Mar 21, 2021 20:00:00',
+        'DSP Poster' => [
+            'id'   => 'dspPoster',
+            'date' => 'June 13, 2021 20:00:00',
         ],
+        'DSP Submission' => [
+            'id'   => 'dspProject',
+            'date' => 'Sept 5, 2021 23:59:59',
+        ],
+        'DSP Viva' => [
+            'id'   => 'dspViva',
+            'date' => 'Sept 7, 2021 00:00:00',
+        ],
+    ],
+    [
         'ESD Sprint 2' => [
             'id'   => 'esdSprint2',
-            'date' => 'Apr 18, 2021 20:00:00',
+            'date' => 'June 6, 2021 20:00:00',
+        ],
+        'ESD UML Crit Review' => [
+            'id'   => 'esdUmlCritReview',
+            'date' => 'June 27, 2021 20:00:00',
         ],
         'ESD Sprint 3' => [
             'id'   => 'esdSprint3',
-            'date' => 'May 30, 2021 20:00:00',
+            'date' => 'July 4, 2021 20:00:00',
         ],
         'ESD GP Final' => [
             'id'   => 'esdGpFinal',
-            'date' => 'Jun 6, 2021 20:00:00',
+            'date' => 'July 15, 2021 20:00:00',
+        ],
+    ],
+    [
+        'Ethics Topic Finalization' => [
+            'id'   => 'ethicsTopicFinalization',
+            'date' => 'June 15, 2021 00:00:00',
+        ],
+        'Ethics Essay Submission' => [
+            'id'   => 'ethicsEssaySubmission',
+            'date' => 'July 31, 2021 23:59:59',
+        ],
+        'Ethics Video Submission' => [
+            'id'   => 'ethicsVideoSubmission',
+            'date' => 'August 3, 2021 20:00:00',
+        ],
+    ],
+    [
+        'Crypto Code' => [
+            'id'   => 'cryptoCodeAssignment',
+            'date' => 'July 1, 2021 20:00:00',
+        ],
+        'Crypto Essay' => [
+            'id'   => 'cryptoEssaySubmission',
+            'date' => 'July 3, 2021 23:59:59',
+        ],
+    ],
+    [
+        'BioComp Assignment' => [
+            'id'   => 'bioCompAssignment',
+            'date' => 'July 31, 2021 20:00:00',
         ],
     ],
 ];
